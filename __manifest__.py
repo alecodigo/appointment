8ffefd41d091c3af9dcc3c1977308b82ffdf3530
Alejandro Sanchez <a.sanchez@solidconsult.cl>
2022-12-11 12:52:26 -0400

    my first commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# coder: Alejandro Sanchez
+
+
+{
+    'name': 'Appointment',
+    'version': '0.1',
+    'website': '',
+    'author': 'Alejandro Sanchez',
+    'description': 'Custom appointment module for Odoo community',
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+    
+    }
+
+
+
